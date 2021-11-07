0b304d452db7ae11ab2199da3c92549704294d26
TheCodeFox-tech <78907865+TheCodeFox-tech@users.noreply.github.com>
2021-11-07 16:19:17 +0100

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/runner/work/CMakeTest/CMakeTest
-# Build directory: /home/runner/work/CMakeTest/CMakeTest/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test "/home/runner/work/CMakeTest/CMakeTest/build/Main")
-set_tests_properties(test PROPERTIES  PASS_REGULAR_EXPRESSION "Hello World!" _BACKTRACE_TRIPLES "/home/runner/work/CMakeTest/CMakeTest/CMakeLists.txt;12;add_test;/home/runner/work/CMakeTest/CMakeTest/CMakeLists.txt;0;")
